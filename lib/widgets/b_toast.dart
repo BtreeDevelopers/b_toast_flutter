9c814b49960462bf845b227f32d43f42bd0b9647
Thalys M <thalyscorreia@hotmail.com>
2023-07-19 23:00:33 -0300

    barra de progresso

--- lib/widgets/b_toast.dart
+++ lib/widgets/b_toast.dart
@@ -36,51 +36,73 @@ class BToast {
             widthFactor: 1,
             child: SlideTransition(
               position: _offsetAnimation,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(Radius.circular(10)),
-                      color: const Color(0xff252831),
-                      border: Border.all(
-                        color: borderColor,
-                        width: 4.0,
-                      ),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          const Expanded(
-                            child: Padding(
-                              padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                              child: Text(
-                                'Explore page',
-                                style: TextStyle(
-                                  color: Color(0xffC6C6C6),
-                                  fontSize: 14.0,
-                                  decoration: TextDecoration.none,
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(10)),
+                          color: const Color(0xff252831),
+                          border: Border.all(
+                            color: borderColor,
+                            width: 4.0,
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              const Expanded(
+                                child: Padding(
+                                  padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                                  child: Text(
+                                    'Explore page',
+                                    style: TextStyle(
+                                      color: Color(0xffC6C6C6),
+                                      fontSize: 14.0,
+                                      decoration: TextDecoration.none,
+                                    ),
+                                  ),
                                 ),
                               ),
-                            ),
+                              GestureDetector(
+                                onTap: () {
+                                  hide();
+                                },
+                                child: const Icon(
+                                  Icons.close,
+                                  color: Color(0xffC6C6C6),
+                                ),
+                              ),
+                            ],
                           ),
-                          GestureDetector(
-                            onTap: () {
-                              hide();
-                            },
-                            child: const Icon(
-                              Icons.close,
-                              color: Color(0xffC6C6C6),
-                            ),
-                          ),
-                        ],
+                        ),
                       ),
                     ),
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: AnimatedPositioned(
+                      duration: Duration(milliseconds: 500),
+                      left: 0,
+                      right: (1.0 - _controller.value) * 200.0,
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 8,
+                        decoration: BoxDecoration(
+                            color: borderColor,
+                            borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(10.0))),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
